eca011eff8484ffa8b0774f91a3181a7f6f37aa2
Charles Peace <charlespeace@hotmail.com>
2022-02-25 21:13:29 -0800

    completed prompt questions and started functions for writeFile and generateMarkdown

---
+++ generateMarkdown.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+// const { generateMarkdown } = require('./index');
+
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+    return `
+    # ${data.title}
+
+    # Table of Contents
+    -[Description](#Description)
+    -[Installation](#Installation)
+    -[Usage](#Usage)
+    -[Contribution](#Contribution)
+    -[Tests](#Tests)
+    -[Questions](#Questions)
+
+    ## Description
+    -${data.description}
+
+    ## Installation
+    -${data.installation}
+
+    ## Usage
+    -${data.usage}
+
+    ## Contribution
+    -${data.contribution}
+
+    ## Tests
+    -${data.test}
+
+    ## Questions
+    -${data.gihub}
+    -${data.email}
+    -${data.contact}
+`;
+};
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = answers => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of the project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide some usage information.',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Who were the contributors to this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide test instructions for the project.',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the github username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please provide an email address for users to contact regarding the project.',
+        },
+        {
+            type: 'input',
+            name: 'contact',
+            message: 'What is the best way for users to reach me with additional questions?',
+        }
+    ])
+    .then(projectData => {
+        answers.push(projectData);
+          return answers;
+    });
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(projectData) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./write', projectData, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {};
 
 // Function call to initialize app
-init();
+questions()
+    .then(generateMarkdown)
