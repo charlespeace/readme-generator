4429f4925b55aef6de3959bcbd7e2d3c1bc05cbc
Charles Peace <charlespeace@hotmail.com>
2022-03-02 13:59:06 -0800

    modified file names to be more accurate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { renderLicenseBadge, renderLicenseSection } = require('./generateMarkdown');
+const { renderLicenseBadge, renderLicenseSection } = require('./renderLicense');
 
 // TODO: Create an array of questions for user input
 const questions = data => {
